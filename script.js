ad2eb2e059c24092ddde4e771959dd6fb0bf25c1
TimurMP <titimurmamyshev@gmail.commur>
2023-02-05 15:28:33 +0200

    F

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 const file = document.getElementById("file")
 const input = document.querySelector("input")
 let imagesArray = []
-input.addEventListener("change", () => {
-    const files = input.files
-    imagesArray.push(files[0])
-    console.log(files[0])
-})
+
 
 let myHeaders = new Headers();
 myHeaders.append("Authorization", "Client-ID 76f00d4ef97eb17");
 
 let formdata = new FormData();
-formdata.append("image", file[0]);
+
+input.addEventListener("change", () => {
+    const files = input.files
+    // imagesArray.push(files[0])
+    console.log(files[0])
+    formdata.append("image", files[0]);
+})
 
 
 var requestOptions = {
@@ -21,10 +23,22 @@ var requestOptions = {
     redirect: 'follow'
 };
 
+
 upload.onclick = e => {
     fetch("https://api.imgur.com/3/upload", requestOptions)
-        .then(response => response.text())
-        .then(result => console.log(result))
+        .then(response => response.json())
+        .then(res => {
+            console.log(res.data.link)
+            console.log(res);
+            const pic = document.createElement('div');
+            pic.appendChild(document.createTextNode(`Link to your image: ${res.data.link}`));
+            picture.appendChild(pic);
+            const img = document.getElementById("img")
+            img.src = res.data.link;
+            // console.log(res.link)
+
+        })
+        // .then(result => console.log(result))
         .catch(error => console.log(error));
 }
 
